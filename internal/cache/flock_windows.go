9907565be066aa39d3f2de9780a93cc8eef291e0
bolens <bolens@duck.com>
2026-08-08 17:18:19 -0600

    fix(cache): lock across Windows processes

---
+++ internal/cache/flock_windows.go
@@ -0,0 +1,26 @@
+//go:build windows
+
+package cache
+
+import (
+	"os"
+
+	"golang.org/x/sys/windows"
+)
+
+func flockExclusive(f *os.File) error {
+	var overlapped windows.Overlapped
+	return windows.LockFileEx(
+		windows.Handle(f.Fd()),
+		windows.LOCKFILE_EXCLUSIVE_LOCK,
+		0,
+		1,
+		0,
+		&overlapped,
+	)
+}
+
+func flockUnlock(f *os.File) error {
+	var overlapped windows.Overlapped
+	return windows.UnlockFileEx(windows.Handle(f.Fd()), 0, 1, 0, &overlapped)
+}
